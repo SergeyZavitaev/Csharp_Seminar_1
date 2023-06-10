e9c7c822bf4f10624c719c77addc17559498585f
Sergey Zavitaev <nikgala@list.ru>
2023-06-10 16:08:10 +0300

    Added solution for Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Random rd = new Random();
+
+Console.WriteLine("DZ zadacha 8:");
+int input = 0;
+Console.WriteLine(input = rd.Next(1,100));
+int count = (input / 2);
+for (int index = 1; index <= count; index++)
+{
+    Console.Write($"{index * 2}, ");
+}
