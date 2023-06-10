1ea3f4e4450796b48444c41960a8970a512daa09
Sergey Zavitaev <nikgala@list.ru>
2023-06-10 15:49:54 +0300

    Added solution for Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Random rd = new Random();
+
+Console.WriteLine("DZ zadacha 2:");
+int input1 = 0;
+int input2 = 0;
+System.Console.WriteLine($"a={input1 = rd.Next(1, 100)}");
+System.Console.WriteLine($"b={input2 = rd.Next(1, 100)}");
+if (input1 > input2){
+    System.Console.WriteLine($"max = {input1}");
+}else{
+    System.Console.WriteLine($"max = {input2}");
+}
