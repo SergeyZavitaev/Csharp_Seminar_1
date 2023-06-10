5fd333d5130494c0ccad2254d43ef65fb5fa9a4d
Sergey Zavitaev <nikgala@list.ru>
2023-06-10 16:05:30 +0300

    Added solution for Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Random rd = new Random();
+
+Console.WriteLine("DZ zadacha 6:");
+int input = 0;
+System.Console.WriteLine(input = rd.Next(-10, 11));
+if (input % 2 == 0)
+{
+    Console.WriteLine("Число чётное!");
+}
+else
+{
+    Console.WriteLine("Число не чётное!");
+}
